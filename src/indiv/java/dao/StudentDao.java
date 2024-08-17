a5d5fa8e02f4f2d4845f4fa918512316c2bd4d04
CyberLelouch <167730646@qq.com>
2024-08-17 11:14:24 +0800

    04 -分支dev01创建students用来存放学生对象

--- src/indiv/java/dao/StudentDao.java
+++ src/indiv/java/dao/StudentDao.java
@@ -1,5 +1,10 @@
 package indiv.java.dao;
 
+import indiv.java.entity.Student;
+
+import java.util.ArrayList;
+
 public class StudentDao {
+    private static ArrayList<Student> students = new ArrayList<>();
 
 }
